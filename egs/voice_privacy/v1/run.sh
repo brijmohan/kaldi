dcd423f02d08503362d15287febf4517c087cb6b
Brij Mohan Lal Srivastava <brij.srivastava@inria.fr>
2020-01-02 15:47:14 +0100

    added README

--- egs/voice_privacy/v1/run.sh
+++ egs/voice_privacy/v1/run.sh
@@ -55,15 +55,15 @@ fi
 
 
 # Extract xvectors from anonymization pool
-if [ $stage -le 6 ]; then
-  echo "Stage 6: Extracting xvectors for anonymization pool."
+if [ $stage -le 0 ]; then
+  echo "Stage 0: Extracting xvectors for anonymization pool."
   local/featex/01_extract_xvectors.sh --nj $nj data/${anoni_pool} ${xvec_nnet_dir} \
 	  ${anon_xvec_out_dir}
 fi
 
 # Make evaluation data
-if [ $stage -le 7 ]; then
-  echo "Stage 7: Making evaluation data"
+if [ $stage -le 1 ]; then
+  echo "Stage 1: Making evaluation data"
   python local/make_librispeech_eval.py ./proto/eval1 ${librispeech_corpus}/test-clean "" || exit 1;
   python local/make_librispeech_eval2.py proto/eval2 ${librispeech_corpus} "" || exit 1;
 
@@ -94,11 +94,9 @@ if [ $stage -le 7 ]; then
   done
 fi
 
-#exit 0;
-
 # Extract xvectors from data which has to be anonymized
-if [ $stage -le 8 ]; then
-  echo "Stage 8: Extracting xvectors for source data."
+if [ $stage -le 2 ]; then
+  echo "Stage 2: Extracting xvectors for source data."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/featex/01_extract_xvectors.sh --nj $nj data/${name} ${xvec_nnet_dir} \
 	  ${anon_xvec_out_dir} || exit 1;
@@ -106,16 +104,16 @@ if [ $stage -le 8 ]; then
 fi
 
 # Extract pitch for source data
-if [ $stage -le 9 ]; then
-  echo "Stage 9: Pitch extraction for source data."
+if [ $stage -le 3 ]; then
+  echo "Stage 3: Pitch extraction for source data."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/featex/02_extract_pitch.sh --nj ${nj} data/${name} || exit 1;
   done
 fi
 
 # Extract PPGs for source data
-if [ $stage -le 10 ]; then
-  echo "Stage 10: PPG extraction for source data."
+if [ $stage -le 4 ]; then
+  echo "Stage 4: PPG extraction for source data."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/featex/extract_ppg.sh --nj $nj --stage 0 ${name} \
 	  ${ivec_extractor} ${ivec_data_dir}/ivectors_${name} \
@@ -124,8 +122,8 @@ if [ $stage -le 10 ]; then
 fi
 
 # Generate pseudo-speakers for source data
-if [ $stage -le 11 ]; then
-  echo "Stage 11: Generating pseudo-speakers for source data."
+if [ $stage -le 5 ]; then
+  echo "Stage 5: Generating pseudo-speakers for source data."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/anon/make_pseudospeaker.sh --rand-level ${pseudo_xvec_rand_level} \
       	  --cross-gender ${cross_gender} \
@@ -135,8 +133,8 @@ if [ $stage -le 11 ]; then
 fi
 
 # Create netcdf data for voice conversion
-if [ $stage -le 12 ]; then
-  echo "Stage 12: Make netcdf data for VC."
+if [ $stage -le 6 ]; then
+  echo "Stage 6: Make netcdf data for VC."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/anon/make_netcdf.sh --stage 0 data/${name} ${ppg_dir}/ppg_${name}/phone_post.scp \
 	  ${anon_xvec_out_dir}/xvectors_${name}/pseudo_xvecs/pseudo_xvector.scp \
@@ -144,23 +142,23 @@ if [ $stage -le 12 ]; then
   done
 fi
 
-if [ $stage -le 13 ]; then
-  echo "Stage 13: Extract melspec from acoustic model for each data."
+if [ $stage -le 7 ]; then
+  echo "Stage 7: Extract melspec from acoustic model for each data."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     local/vc/am/01_gen.sh ${data_src_netcdf}/${name} || exit 1;
   done
 fi
 
-if [ $stage -le 14 ]; then
-  echo "Stage 14: Generate waveform from NSF model for each data."
+if [ $stage -le 8 ]; then
+  echo "Stage 8: Generate waveform from NSF model for each data."
   #for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
   for name in $eval1_trial $eval2_enroll $eval2_trial; do
     local/vc/nsf/01_gen.sh ${data_src_netcdf}/${name} || exit 1;
   done
 fi
 
-if [ $stage -le 15 ]; then
-  echo "Stage 15: Creating new data directories corresponding to anonymization."
+if [ $stage -le 9 ]; then
+  echo "Stage 9: Creating new data directories corresponding to anonymization."
   for name in $eval1_enroll $eval1_trial $eval2_enroll $eval2_trial; do
     wav_path=${data_src_netcdf}/${name}/nsf_output_wav
     new_data_dir=data/${name}${anon_data_suffix}
@@ -171,8 +169,8 @@ if [ $stage -le 15 ]; then
   done
 fi
 
-if [ $stage -le 16 ]; then
-  echo "Stage 16: Evaluate the dataset using speaker verification."
+if [ $stage -le 10 ]; then
+  echo "Stage 10: Evaluate the dataset using speaker verification."
   #echo "Exp 0.1 baseline: Eval 1, enroll - original, trial - original"
   #local/eval_libri.sh ${eval1_enroll} ${eval1_trial} || exit 1;
   #echo "Exp 0.2 baseline: Eval 2, enroll - original, trial - original"
@@ -186,7 +184,5 @@ if [ $stage -le 16 ]; then
   echo "Exp 4: Eval 2, enroll - anonymized, trial - anonymized"
   local/eval_libri.sh ${eval2_enroll}${anon_data_suffix} ${eval2_trial}${anon_data_suffix} || exit 1;
 fi
-
-
 
 
